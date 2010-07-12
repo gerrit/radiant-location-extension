87f7fe9f3edb1f0a8e5bff7d3d146a78a574b031
Gerrit Kaiser <gerrit@gerritkaiser.de>
2010-07-12 09:26:33 +0100

    use define_method instead of string eval

--- lib/location_geo_kit.rb
+++ lib/location_geo_kit.rb
@@ -1,32 +1,36 @@
-[:default_units, :default_formula].each do |sym|
-  GeoKit.class_eval <<-EOS, __FILE__, __LINE__
-  def self.#{sym}
-#    puts "My #{sym.to_s}"
-    Radiant::Config["geokit.#{sym.to_s}"].to_sym
-  end
-
-  def self.#{sym}=(obj)
-    Radiant::Config["geokit.#{sym.to_s}"] = obj
-  end
-  EOS
-end
-[:yahoo, :google, :geocoder_us, :geocoder_ca, :proxy_addr, :proxy_port, :proxy_user, :proxy_pass].each do |sym|
-  GeoKit::Geocoders.class_eval <<-EOS, __FILE__, __LINE__
-  def self.#{sym}
-#    puts "My #{sym.to_s}"
-    val = Radiant::Config["geokit.geocoders.#{sym.to_s}"]
-    if val.strip.downcase.eql?("nil")
-      nil
-    else
-      val
+GeoKit.class_eval do
+  class << self
+    [:default_units, :default_formula].each do |sym|
+      define_method sym do
+        Radiant::Config["geokit.#{sym}"].to_sym
+      end
+      
+      define_method "#{sym}=" do |obj|
+        Radiant::Config["geokit.#{sym}"] = obj
+      end
     end
   end
-
-  def self.#{sym}=(obj)
-    Radiant::Config["geokit.geocoders.#{sym.to_s}"] = obj
-  end
-  EOS
 end
+
+GeoKit::Geocoders.class_eval do
+  class << self
+    [:yahoo, :google, :geocoder_us, :geocoder_ca, :proxy_addr, :proxy_port, :proxy_user, :proxy_pass].each do |sym|
+      define_method sym do
+        val = Radiant::Config["geokit.geocoders.#{sym}"]
+        if val.strip.downcase.eql?("nil")
+          nil
+        else
+          val
+        end
+      end
+      
+      define_method "#{sym}=" do |obj|
+        Radiant::Config["geokit.geocoders.#{sym}"] = obj
+      end
+    end
+  end
+end
+
 GeoKit::Geocoders.class_eval do
   def self.provider_order
     #    puts Radiant::Config["geokit.geocoders.provider_order"]
