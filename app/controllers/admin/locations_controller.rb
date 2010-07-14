083fbbb61589e9a709e5507a81ef1733bd8bc30a
Gerrit Kaiser <gerrit@gerritkaiser.de>
2010-07-14 09:15:04 +0100

    use page edit/remove functions for locations that are associated with a page

--- app/controllers/admin/locations_controller.rb
+++ app/controllers/admin/locations_controller.rb
@@ -1,3 +1,16 @@
 class Admin::LocationsController < Admin::ResourceController
+  helper 'admin/location'
   model_class 'Location'
+  before_filter :redirect_to_page_admin
+private
+  def redirect_to_page_admin
+    if model.page
+      case params[:action]
+      when 'edit'
+        redirect_to edit_admin_page_path(model.page)
+      when 'remove'
+        redirect_to remove_admin_page_path(model.page)
+      end
+    end
+  end
 end
--- app/helpers/admin/location_helper.rb
+++ app/helpers/admin/location_helper.rb
@@ -1,2 +1,11 @@
 module Admin::LocationHelper
+  def link_to_location_edit_form(location)
+    href = location.page ? edit_admin_page_path(location.page) : edit_admin_location_path(location)
+    link_to location.name, href
+  end
+  
+  def link_to_location_removal_page(location)
+    href = location.page ? remove_admin_page_path(location.page) : remove_admin_location_path(location)
+    link_to 'Remove', href
+  end
 end
